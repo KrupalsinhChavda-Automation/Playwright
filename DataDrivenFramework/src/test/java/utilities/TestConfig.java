ff4618a2b045c7f92d107d10d1288537d51e31f4
KrupalsinhChavda-Automation <krupalchavda3499@gmail.com>
2025-04-19 12:27:14 +0530

    New POM project commit

--- DataDrivenFramework/src/test/java/utilities/TestConfig.java
+++ DataDrivenFramework/src/test/java/utilities/TestConfig.java
@@ -5,18 +5,18 @@ public class TestConfig{
 
 	
 	public static String server="smtp.gmail.com";
-	public static String from = "java@way2automation.com";
-	public static String password = "Selenium@123";
-	public static String[] to ={"seleniumcoaching@gmail.com","trainer@way2automation.com"};
+	public static String from = "test@test.com"; // Change mails as per your config
+	public static String password = "test@123";
+	public static String[] to ={"test01@gmail.com","test@02.com"};
 	public static String subject = "Test Report";
 	
 	public static String messageBody ="TestMessage";
-	public static String attachmentPath="C:\\Users\\way2automation\\Desktop\\Desktop\\newyear.png";
-	public static String attachmentName="error.jpg";
+	public static String attachmentPath="//*.html";
+	public static String attachmentName="Extent Report";
 	
 
 	
-	//MYSQL DATABASE DETAILS
+	//MYSQL DATABASE DETAILS // Setup your database config
 	public static String mysqldriver="com.mysql.cj.jdbc.Driver";
 	public static String mysqluserName = "root";
 	public static String mysqlpassword = "selenium";
